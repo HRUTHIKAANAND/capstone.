d54d87edb2172788fd31e5ecc49930f98adb73a1
Hruthika <hruthikaba07@gmail.com>
2022-09-02 07:40:14 +0530

    capstone frontend

---
+++ angular-Framework/src/app/discount.ts
@@ -0,0 +1,16 @@
+export class Discount {
+
+    did:number;
+    id:number;
+    percentage:number;
+    image:string;
+    
+    constructor(did:number,id:number,percentage:number,image:string){
+        this.did=did;
+        this.id=id;
+        this.percentage=percentage;
+        this.image=image;
+
+
+    }
+}
